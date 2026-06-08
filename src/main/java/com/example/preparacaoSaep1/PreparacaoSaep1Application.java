275ad4a0e75ac2db4b86ecd3bd13bec22c9d6cfa
henzo119 <henzo_araujo@estudante.sesisenai.org.br>
2026-06-08 08:45:08 -0300

    Initial commit

---
+++ src/main/java/com/example/preparacaoSaep1/PreparacaoSaep1Application.java
@@ -0,0 +1,13 @@
+package com.example.preparacaoSaep1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreparacaoSaep1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreparacaoSaep1Application.class, args);
+	}
+
+}
